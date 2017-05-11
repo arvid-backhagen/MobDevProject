d05f8a3d0aae513069e372fed43510ce53dc8e0c
arvidlarzzon <arvidlarzzon@hotmail.com>
2017-05-11 18:22:00 +0200

    Ajax fucking aroudn

--- javascript/model.js
+++ javascript/model.js
@@ -6,23 +6,7 @@ var Model = function() {
 		$.ajax( {
 			url: 'https://www.kth.se/api/places/v3/room/id/0e2af584-d1d1-40c8-83b1-e3fdb285dcc5',
 			headers: {
-				'name': 'api_key',
-				'in': 'header',
-				'value': 'arjYt564HFYiiias342z'
-			},
-			success: function(data) {
-				console.log(data)
-				cb(data);
-			}
-		})
-	}
-
-	this.getRoom = function (cb) {
-		$.ajax( {
-			url: 'https://www.kth.se/api/places/v3/room/id/0e2af584-d1d1-40c8-83b1-e3fdb285dcc5',
-			headers: {
-				'name': 'api_key',
-				'value': 'arjYt564HFYiiias342z'
+				'api_key': 'arjYt564HFYiiias342z'
 			},
 			success: function(data) {
 				console.log(data)
@@ -33,7 +17,7 @@ var Model = function() {
 }
 
 var model = new Model();
-var first = model.getAllRooms();
-var second = model.getRoom();
-console.log(first);
-console.log(second);
+model.getAllRooms(function(room){
+	var najs = room;
+	console.log(najs)
+});
